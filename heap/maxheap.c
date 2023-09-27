d2a0bf2c67f5a60f09bfb2bea2579974adbb2220
Rishi Kumar <rishikumar10dec@gmail.com>
2023-09-27 08:23:00 +0530

    heapsort working

--- heap/maxheap.c
+++ heap/maxheap.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int size;
-int n = -1;
+int max_size;
+int last_index = -1;
 
 int parent(int i){
     return (i - 1)/2;
@@ -18,7 +18,7 @@ int rightChild(int i){
 
 void printHeap(int *heap){
     printf("\nHeap is as follows:\n");
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i <= last_index; i++)
     {
         printf("%d\t", heap[i]);
     }
@@ -38,12 +38,12 @@ void shiftup(int *heap, int i){
 void shiftdown(int *heap, int i){
     int max_index = i;
     int left = leftChild(i);
-    if (left <= size && heap[left] > heap[max_index])
+    if (left <= last_index && heap[left] > heap[max_index])
     {
         max_index = left;
     }
     int right = rightChild(i);
-    if (right <= size && heap[right] > heap[max_index])
+    if (right <= last_index && heap[right] > heap[max_index])
     {
         max_index = right;
     }
@@ -58,30 +58,30 @@ void shiftdown(int *heap, int i){
 }
 
 int extractMax(int *heap){
-    if (n == -1)
+    if (last_index == -1)
     {
         printf("Heap Empty!");
     }
     int result = heap[0];
-    heap[0] = heap[n];
-    n--;
+    heap[0] = heap[last_index];
+    last_index--;
     shiftdown(heap, 0);
     return result;
 }
 
 void insert(int *heap){
-    if (n >= size - 1)
+    if (last_index >= max_size - 1)
     {
         printf("Heap is full\n");
         return;
     }
-    n++;
-    scanf("%d", &heap[n]);
-    shiftup(heap, n);
+    last_index++;
+    scanf("%d", &heap[last_index]);
+    shiftup(heap, last_index);
 }
 
 void removeElement(int *heap, int i){
-    if (i > n)
+    if (i > last_index)
     {
         printf("Out of bound removal!\n");
         return;
@@ -92,7 +92,7 @@ void removeElement(int *heap, int i){
 }
 
 void changePriority(int *heap, int i, int new){
-    if (i > n)
+    if (i > last_index)
     {
         printf("Out of bound!\n");
         return;
@@ -109,37 +109,36 @@ void changePriority(int *heap, int i, int new){
 }
 
 
-// void buildHeap(int *arr, int num){
-//     int size = num;
-//     for (int i = (size-1)/2; i <= 0 ; i--)
-//     {
-//         shiftdown(arr, i);
-//     }
-// }
+void buildHeap(int *arr, int arr_size){
+    for (int i = (arr_size-1)/2; i >= 0 ; i--)
+    {
+        shiftdown(arr, i);
+    }
+}
 
-// void heapSort(int *arr, int num){
-//     buildHeap(arr, num);
-//     int size = num - 1;
-//     while((size)--){
-//         int temp = arr[size];
-//         arr[size] = arr[0];
-//         arr[0] = temp;
-//         shiftdown(arr, 0);
-//     }
-// }
+void heapSort(int *arr, int arr_size){
+    buildHeap(arr, arr_size);
+    while(last_index > 0){
+        int temp = arr[last_index];
+        arr[last_index] = arr[0];
+        arr[0] = temp;
+        last_index--;
+        shiftdown(arr, 0);
+    }
+}
 
 
 int main(){
-    printf("Enter the max size of heap: ");
-    scanf("%d", &size);
-    int *heap = (int *)malloc(size);
+    // printf("Enter the max size of heap: ");
+    // scanf("%d", &max_size);
+    // int *heap = (int *)malloc(max_size);
 
-    insert(heap);
-    insert(heap);
-    insert(heap);
-    insert(heap);
+    // insert(heap);
+    // insert(heap);
+    // insert(heap);
+    // insert(heap);
 
-    printHeap(heap);
+    // printHeap(heap);
 
     // printf("Max element is: %d\n", extractMax(heap));
     // printHeap(heap);
@@ -147,25 +146,30 @@ int main(){
     // removeElement(heap, 1);
     // printHeap(heap);
 
-    changePriority(heap, 3, 67);
-    printHeap(heap);
+    // changePriority(heap, 1, 67);
+    // printHeap(heap);
 
-    /* sorting testing --> // NOT WORKING // */ 
-    // int m;
-    // printf("Enter the max size of array: ");
-    // scanf("%d", &m);
-    // int *arr = (int *)malloc(sizeof(int)*m);
 
-    // for (int i = 0; i < m; i++)
-    // {
-    //     scanf("%d", &arr[i]);
-    // }
+
+    /* sorting testing --> // WORKING // */
+    printf("Enter the max size of heap: ");
+    scanf("%d", &max_size);
+    int *arr = (int *)malloc(max_size);
+
+    int m = 10;
+    last_index = m - 1;
+
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
     
-
-    // heapSort(arr, m);
-    // for (int i = 0; i < m; i++)
-    // {
-    //     printf("%d\t", arr[i]);
-    // }
-    // printf("\n");
+    heapSort(arr, m);
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
 }
